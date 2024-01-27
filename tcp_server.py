cd832a2f2fb2656d1603213995ed4ee487a66c64
EthanVillanti <43498184+EthanVillanti@users.noreply.github.com>
2024-01-27 11:10:17 -0600

    Update tcp_server.py
    finalized tcp server to acceptable level

--- tcp_server.py
+++ tcp_server.py
@@ -15,36 +15,27 @@ client, address = server.accept()
 client.send("Hello Client!".encode('utf-8'))
 print(f"Connected to {address}")
 consecutiveDroppedPackets = 0
-counter = 1
 
 
 
 
 while True:
-    #randomly drop 20% of packets 
-    randomNum = randrange(1,5)
-    if randomNum == 3: 
-        print("simulated packet drop")
-        continue
     
     # we know data payload, "seq #" + [sequence #], "timestamp" + [timestamp]
     tokens = client.recv(1024).decode('utf-8').split()
+    
+    # if we didn't get anything from the client just keep listening 
     if not tokens:
-        print(f"didn't get a packet, expexting seq num {counter}")
+        print("Didn't get a packet")
         consecutiveDroppedPackets += 1
-        if consecutiveDroppedPackets >= 5: 
-            print("5 consecutive dropped packets. Exiting")
+        if consecutiveDroppedPackets >= 5:
             break
-        else:
-            counter += 1
-            continue
-    consecutiveDroppedPackets = 0
+        continue
+
     seqNum = tokens[2]
-    timeStamp = tokens[4]
-    if int(counter) == int(seqNum):
-        client.send(f"Ack {seqNum}".encode('utf-8'))
-    else: 
-        print("dropped packet")
-        #increment droppedPackets variable? do we care about packets 
-    counter += 1  
+    if randrange(1,5) == 3:
+        print("Simulated packet drop, clobbering sequence number")
+        seqNum = int(seqNum) +1
+    client.send(f"Ack {seqNum}".encode('utf-8'))
+
 server.close
